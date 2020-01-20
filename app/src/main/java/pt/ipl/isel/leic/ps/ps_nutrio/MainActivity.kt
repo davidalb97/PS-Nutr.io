3d79faa8342ba402e7f00127283872d622fe0ca1
davidalb97 <davidalb97@hotmail.com>
2020-01-20 23:15:23 +0000

    Initial project setup
    Signed-off-by: davidalb97 <davidalb97@hotmail.com>

---
+++ app/src/main/java/pt/ipl/isel/leic/ps/ps_nutrio/MainActivity.kt
@@ -0,0 +1,12 @@
+package pt.ipl.isel.leic.ps.ps_nutrio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
