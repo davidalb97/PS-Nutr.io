22db32f36882d473d3686cda08b7ef74423e7fe3
Mig07 <miguelfluis7@gmail.com>
2020-04-04 19:39:14 +0100

    Database model creation updated, folder with SQL access functions added

---
+++ database/SQL_functions/portion_submission_functions.sql
@@ -0,0 +1,31 @@
+-- ############################## SubmissionPortion insertion function ############################## - Compiles
+CREATE FUNCTION portionSubmissionInsertion
+(
+	_submission_type varchar(5),
+	_submitter_id integer,
+	_quantity integer,
+	_meal_id integer
+) RETURNS VOID AS $$	
+	BEGIN
+		INSERT INTO SubmissionSubmitter(submission_type, submitter_id) VALUES
+		(_submission_type, _submitter_id);
+	
+		IF NOT EXISTS (
+			SELECT * FROM SubmissionMeal WHERE meal_id = _meal_id -- TODO: INNER JOIN
+		) THEN			
+			RAISE NOTICE 'The meal where you are associating the portion does not exist.';
+			ROLLBACK;
+		END IF;
+	
+		INSERT INTO SubmissionPortion(meal_id, quantity) VALUES 
+			(_meal_id, _quantity); 
+		
+	END; $$
+	LANGUAGE PLPGSQL;
+	
+	
+-- ############################## SubmissionPortion deletion function ##############################
+-- TODO
+
+-- ############################## SubmissionPortion update function ##############################
+-- TODO
---
+++ database/SQL_functions/restaurant_submission_functions.sql
@@ -0,0 +1,45 @@
+-- ############################## SubmissionRestaurant insertion function ############################## - Compiles
+CREATE FUNCTION restaurantSubmissionInsertion
+(
+	_submission_type varchar(5),
+	_submitter_id integer,
+	_restaurant_name varchar(100),
+	_latitude real,
+	_longitude real
+) RETURNS VOID AS $$
+	BEGIN
+		IF EXISTS (
+			SELECT * FROM Restaurant WHERE restaurant_name = _restaurant_name AND
+			latitude = _latitude AND longitude = _longitude
+		) THEN			
+			RAISE NOTICE 'The inserted restaurant already exists in the database.';
+		END IF;
+        INSERT INTO SubmissionSubmitter(submission_type, submitter_id) VALUES
+		(_submission_type, _submitter_id);
+		INSERT INTO Restaurant(restaurant_name, latitude, longitude) VALUES
+		(_restaurant_name, _latitude, _longitude);		
+	END; $$
+	LANGUAGE PLPGSQL;
+	
+-- ############################## SubmissionRestaurant deletion function ############################## - Not complete
+CREATE FUNCTION restaurantSubmissionDeletion
+(	
+	_submitter_id integer,
+	_restaurant_id integer,
+) RETURNS VOID AS $$
+	BEGIN
+		IF NOT EXISTS (
+			SELECT * FROM Restaurant WHERE restaurant_name = _restaurant_name AND
+			latitude = _latitude AND longitude = _longitude
+		) THEN			
+			RAISE NOTICE 'The inserted restaurant already exists in the database.';
+		END IF;
+        INSERT INTO SubmissionSubmitter(submission_type, submitter_id) VALUES
+		(_submission_type, _submitter_id);
+		INSERT INTO Restaurant(restaurant_name, latitude, longitude) VALUES
+		(_restaurant_name, _latitude, _longitude);		
+	END; $$
+	LANGUAGE PLPGSQL;
+
+-- ############################## SubmissionRestaurant update function ##############################
+-- TODO
