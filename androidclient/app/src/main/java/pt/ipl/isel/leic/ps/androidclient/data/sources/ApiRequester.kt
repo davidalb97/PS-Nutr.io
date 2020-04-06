ecfbb43b73914663ff1ec82801d808c9b16e72d5
Mig07 <miguelfluis7@gmail.com>
2020-04-06 16:39:37 +0100

    Started android app, new icons and dependencies added, added data package

---
+++ androidclient/app/src/main/java/pt/ipl/isel/leic/ps/androidclient/data/sources/ApiRequester.kt
@@ -0,0 +1,9 @@
+package pt.ipl.isel.leic.ps.androidclient.data.sources
+
+const val ADDRESS = "";
+const val PORT = "";
+// TODO - Each path/query for each endpoint
+
+class ApiRequester : ApiEndpoints {
+
+}
