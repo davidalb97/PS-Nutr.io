f3685d017e24d50b3cb66ead73c5309979df82ee
davidalb97 <davidalb97@hotmail.com>
2020-04-19 22:57:20 +0100

    httpserver: ApiDao can now obtain submission id from name as API names are unique
    Signed-off-by: davidalb97 <davidalb97@hotmail.com>

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/dao/ApiDao.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/dao/ApiDao.kt
@@ -3,10 +3,17 @@ package pt.isel.ps.g06.httpserver.dataAccess.db.dao
 import org.jdbi.v3.sqlobject.customizer.Bind
 import org.jdbi.v3.sqlobject.statement.SqlQuery
 import pt.isel.ps.g06.httpserver.dataAccess.db.concrete.DbAPI
+import pt.isel.ps.g06.httpserver.dataAccess.db.concrete.DbSubmissionSubmitter
 
+//API table constants
 private const val table = "API"
 private const val id = "submitter_id"
 private const val apiToken = "api_token"
+
+//Submitter table constants
+private const val submitterTable = "Submitter"
+private const val submitterName = "submitter_name"
+private const val submitterId = "submitter_id"
 
 interface ApiDao {
     @SqlQuery("SELECT * FROM $table")
@@ -18,4 +25,12 @@ interface ApiDao {
     @SqlQuery("INSERT INTO $table($id, $apiToken) " +
             "VALUES(:submitterId, :apiToken)")
     fun insert(@Bind submitterId: Int, @Bind apiToken: String)
+
+    @SqlQuery("SELECT $submitterTable.$submitterId" +
+            " FROM $submitterTable" +
+            " INNER JOIN $table" +
+            " ON $submitterTable.$submitterId = $table.$id" +
+            " WHERE $submitterName = :submitterName"
+    )
+    fun getIdByName(@Bind submitterName: String): Int
 }
