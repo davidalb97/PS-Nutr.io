73e1c0c576a3f4400812effe8d2af28c7be3bc6e
Mig07 <miguelfluis7@gmail.com>
2020-04-19 16:18:11 +0100

    Fixed ApiSubmissionDao and RestaurantDao

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/dao/ApiSubmissionDao.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/dao/ApiSubmissionDao.kt
@@ -6,17 +6,22 @@ import org.jdbi.v3.sqlobject.statement.SqlQuery
 import pt.isel.ps.g06.httpserver.dataAccess.db.concrete.DbAPI_Submission
 import pt.isel.ps.g06.httpserver.dataAccess.db.concrete.DbSubmission
 
+private const val apiSubmission = "API_Submission"
+private const val submissionId = "submission_id"
+private const val apiId = "apiId"
+private const val submissionType = "submission_type"
+
 interface ApiSubmissionDao {
-    @SqlQuery("SELECT * FROM DbAPI_Submission")
+    @SqlQuery("SELECT * FROM $apiSubmission")
     fun getApiSubmissions(): List<DbAPI_Submission>
 
-    @SqlQuery("SELECT * FROM DbAPI_Submission WHERE submission_id = :submissionId")
+    @SqlQuery("SELECT * FROM $apiSubmission WHERE $submissionId = :submissionId")
     fun getApiSubmission(submissionId: Int): DbAPI_Submission
 
-    @SqlQuery("SELECT * FROM DbAPI_Submission WHERE apiId = :apiId")
+    @SqlQuery("SELECT * FROM $apiSubmission WHERE $apiId = :apiId")
     fun getApiSubmissionFromSubmitter(apiId: Int): List<DbAPI_Submission>
 
-    @SqlQuery("INSERT INTO DbAPI_Submission(submission_id, apiId, submission_type) VALUES(:submissionId, :apiId, :submissionType)")
+    @SqlQuery("INSERT INTO $apiSubmission($submissionId, $apiId, $submissionType) VALUES(:submissionId, :apiId, :submissionType)")
     @GetGeneratedKeys
     fun insertSubmission(@Bind submissionId: Int, apiId: Int, submissionType: String): Int
 }
