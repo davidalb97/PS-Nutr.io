baf7bebdc11d47d83506fc52a9b2ece5c28c8224
davidalb97 <davidalb97@hotmail.com>
2020-04-19 23:01:51 +0100

    httpserver: DbMealRepository.newMeal() implemented, SubmissionType and SubmitterType
    Signed-off-by: davidalb97 <davidalb97@hotmail.com>

---
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/SubmissionType.kt
@@ -0,0 +1,8 @@
+package pt.isel.ps.g06.httpserver.dataAccess.db
+
+enum class SubmissionType {
+    Restaurant,
+    Portion,
+    Meal,
+    Ingredient
+}
--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/repo/DbMealRepository.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/repo/DbMealRepository.kt
@@ -3,6 +3,9 @@ package pt.isel.ps.g06.httpserver.dataAccess.db.repo
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.transaction.TransactionIsolationLevel
 import org.springframework.stereotype.Repository
+import pt.isel.ps.g06.httpserver.dataAccess.api.food.FoodApiType
+import pt.isel.ps.g06.httpserver.dataAccess.db.SubmissionType
+import pt.isel.ps.g06.httpserver.dataAccess.db.dao.*
 
 @Repository
 class DbMealRepository(private val jdbi: Jdbi) {
@@ -10,36 +13,43 @@ class DbMealRepository(private val jdbi: Jdbi) {
     fun newMeal(
             submitterId: Int,
             mealName: String,
-            cuisine: String,
-            ingredientIds: Array<Int> = emptyArray(),
-            restaurantId: Int? = null
+            apiId: Int? = null,
+            cuisines: List<String> = emptyList(),
+            ingredientIds: List<Int> = emptyList(),
+            foodApi: FoodApiType? = null
     ): Boolean {
         return jdbi.open().use { handle ->
             return handle.inTransaction<Boolean, Exception>(TransactionIsolationLevel.SERIALIZABLE) {
 
-                /*
+                val submissionDao = it.attach(SubmissionDao::class.java)
+                val submissionId = submissionDao.insert(SubmissionType.Meal.name)
+
+                val submissionSubmitterDao = it.attach(SubmissionSubmitterDao::class.java)
+                submissionSubmitterDao.insert(submissionId, submitterId)
+
                 val mealDao = it.attach(MealDao::class.java)
-                val meals = mealDao.getMealsByName(mealName)
+                mealDao.insert(submissionId, mealName)
 
-                var targetMealID: Int? = null
+                val cuisineDao = it.attach(CuisineDao::class.java)
+                cuisines.forEach(cuisineDao::insert)
 
-                //If a meal must be inserted
-                if(ingredientIds.isNotEmpty() || meals.isEmpty() || ) {
-                    targetMealID = mealDao.insertMeal(mealName)
-                } else {
-                    targetMealID = meals.
-                }
-
-                //Is submission
-                if(restaurantId != null) {
-                    //Restaurant must exist
-                    if(it.attach(RestaurantDao::class.java).getRestaurantById(restaurantId) == null) {
-                        throw MissingRestaurantException(restaurantId)
+                if(foodApi != null) {
+                    //A meal made out of ingredients does not have apiId as the meal itself does not exist
+                    if(apiId == null) {
+                        val mealIngredientDao = it.attach(MealIngredientDao::class.java)
+                        ingredientIds.forEach{ mealIngredientDao.insert(it, submissionId) }
                     }
+                    //If the meal originated from an external API
+                    else {
+                        val apiDao = it.attach(ApiDao::class.java)
+                        val apiSubmitterId = apiDao.getIdByName(foodApi.toString())
+                        submissionSubmitterDao.insert(submissionId, apiSubmitterId)
 
-
+                        val apiSubmissionDao = it.attach(ApiSubmissionDao::class.java)
+                        apiSubmissionDao.insert(submissionId, apiId, foodApi.toString())
+                    }
                 }
-                */
+
                 true
             }
         }
