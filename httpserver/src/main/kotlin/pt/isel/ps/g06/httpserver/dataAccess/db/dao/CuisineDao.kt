22c96db323f0892b218e5ce5bab3c80712f0a862
davidalb97 <davidalb97@hotmail.com>
2020-04-19 16:03:48 +0100

    httpserver: Updated CuisineDao
    Signed-off-by: davidalb97 <davidalb97@hotmail.com>

---
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/dao/CuisineDao.kt
@@ -0,0 +1,19 @@
+package pt.isel.ps.g06.httpserver.dataAccess.db.dao
+
+import org.jdbi.v3.sqlobject.customizer.Bind
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys
+import org.jdbi.v3.sqlobject.statement.SqlQuery
+import pt.isel.ps.g06.httpserver.dataAccess.db.concrete.DbCuisine
+
+private const val table = "Cuisine"
+private const val name = "cuisine_name"
+
+interface CuisineDao {
+
+    @SqlQuery("INSERT INTO $table($name) VALUES(:name)")
+    @GetGeneratedKeys
+    fun insertCuisine(@Bind name: String): Int
+
+    @SqlQuery("SELECT * FROM $table WHERE $name = :name")
+    fun getCuisinesByName(@Bind name: String): List<DbCuisine>
+}
