d575e6184cad8525f47c5b0ddbd020b33bd7211c
davidalb97 <davidalb97@hotmail.com>
2020-04-26 17:21:24 +0100

    Added missing InvalidInputException

---
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/exception/InvalidInputException.kt
@@ -0,0 +1,16 @@
+package pt.isel.ps.g06.httpserver.exception
+
+/**
+ * Exception thrown when a client provides wrong input on a HTTP-request body
+ *
+ * @param domain let's the user what resource the error links to, e.g: 'project'.
+ * @param detail provides extra information about what parameter(s) are missing.
+ */
+class InvalidInputException(val domain: InvalidInputDomain, val detail : String) : Exception()
+
+enum class InvalidInputDomain(val domain: String) {
+    AUTHENTICATION("authentication"),
+    SUBMITTER("submitter"),
+    SUBMISSION("submission"),
+    API("api")
+}
