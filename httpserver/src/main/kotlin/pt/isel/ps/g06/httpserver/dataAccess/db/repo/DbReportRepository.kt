8062aa831cb975f03be38cd382a23ba67563b5f2
Mig07 <miguelfluis7@gmail.com>
2020-04-27 22:14:34 +0100

    Moved methods that were inside DbRestaurantRepository to these 2 repositories

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/repo/DbReportRepository.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/repo/DbReportRepository.kt
@@ -3,22 +3,32 @@ package pt.isel.ps.g06.httpserver.dataAccess.db.repo
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.transaction.TransactionIsolationLevel
 import pt.isel.ps.g06.httpserver.dataAccess.db.dao.ReportDao
+import pt.isel.ps.g06.httpserver.dataAccess.db.dao.SubmissionDao
+import pt.isel.ps.g06.httpserver.dataAccess.db.dto.ReportDto
 
 class DbReportRepository(private val jdbi: Jdbi) {
 
     val serializable = TransactionIsolationLevel.SERIALIZABLE
-    val reportDao = ReportDao::class.java
 
-    fun insert(
+    fun addReport(
             submitterId: Int,
-            submissionId: Int,
-            description: String
-    ): Boolean {
+            submission_id: Int,
+            report: String
+    ): ReportDto? {
         return inTransaction(jdbi, serializable) {
-            val reportDao = it.attach(reportDao)
-            reportDao.insert(submitterId, submissionId, description)
+            //validateSubmitterId(it, submitterId)
 
-            true
+            // Check if the submission exists
+            val submissionDto = it.attach(SubmissionDao::class.java)
+                    .getById(submission_id)
+
+            if (submissionDto != null) {
+                // Submit a report to that Submission
+                return@inTransaction it.attach(ReportDao::class.java)
+                        .insert(submitterId, submission_id, report)
+            }
+
+            null
         }
     }
 }
