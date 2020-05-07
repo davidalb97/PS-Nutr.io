b4c7800a0fecf6c37ee4aedf5ff2dbc2479d963f
Mig07 <miguelfluis7@gmail.com>
2020-05-07 22:58:07 +0100

    RestaurantFragment now extends from the new generic recycler fragment

--- androidclient/app/src/main/java/pt/ipl/isel/leic/ps/androidclient/ui/fragment/RestaurantFragment.kt
+++ androidclient/app/src/main/java/pt/ipl/isel/leic/ps/androidclient/ui/fragment/RestaurantFragment.kt
@@ -7,15 +7,25 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import pt.ipl.isel.leic.ps.androidclient.NutrioApp
 import pt.ipl.isel.leic.ps.androidclient.R
+import pt.ipl.isel.leic.ps.androidclient.data.source.model.Restaurant
+import pt.ipl.isel.leic.ps.androidclient.ui.adapter.CuisineRecyclerAdapter
+import pt.ipl.isel.leic.ps.androidclient.ui.adapter.RestaurantRecyclerAdapter
+import pt.ipl.isel.leic.ps.androidclient.ui.viewmodel.CuisineRecyclerViewModel
 import pt.ipl.isel.leic.ps.androidclient.ui.viewmodel.RestaurantRecyclerViewModel
 
-class RestaurantFragment : Fragment(){
+class RestaurantFragment : RecyclerListFragment<Restaurant>(){
 
+    private val adapter: RestaurantRecyclerAdapter by lazy {
+        RestaurantRecyclerAdapter(viewModel as RestaurantRecyclerViewModel, this.requireContext())
+    }
 
-    lateinit var viewModel: RestaurantRecyclerViewModel
-
+    /**
+     * ViewModel factory
+     * Initializes the view model
+     */
     private fun getViewModelFactory() = object : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
@@ -27,6 +37,20 @@ class RestaurantFragment : Fragment(){
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        ViewModelProvider(this.requireActivity(), getViewModelFactory())
+            .get(RestaurantRecyclerViewModel::class.java)
         return inflater.inflate(R.layout.restaurant_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerList(view)
+        list.adapter = adapter
+        list.layoutManager = LinearLayoutManager(this.requireContext())
+        (viewModel as RestaurantRecyclerViewModel)
+            .getRestaurants(successFunction(), errorFunction())
+        startObserver()
+        startScrollListener()
     }
 }
