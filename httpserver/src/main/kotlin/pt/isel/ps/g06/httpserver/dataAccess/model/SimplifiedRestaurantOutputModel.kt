7cdcb704c6540e68e6f00ca8ed876a70f93345c1
Pedro Pires <pedrokase@hotmail.com>
2020-05-11 14:36:00 +0100

    Nearby restaurant search now only returns a simplified view, instead of fetching it's cuisines and meals

---
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/model/SimplifiedRestaurantOutputModel.kt
@@ -0,0 +1,20 @@
+package pt.isel.ps.g06.httpserver.dataAccess.model
+
+import pt.isel.ps.g06.httpserver.model.Restaurant
+
+data class SimplifiedRestaurantOutputModel(
+        val identifier: String,
+        val name: String,
+        val latitude: Float,
+        val longitude: Float
+)
+
+
+fun toSimplifiedRestaurant(restaurant: Restaurant): SimplifiedRestaurantOutputModel {
+    return SimplifiedRestaurantOutputModel(
+            identifier = restaurant.identifier,
+            name = restaurant.name,
+            latitude = restaurant.latitude,
+            longitude = restaurant.longitude
+    )
+}
