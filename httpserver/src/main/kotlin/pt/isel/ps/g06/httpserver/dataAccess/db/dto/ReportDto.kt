97c69b254e772d06662aa945cd17be6e4c5151a0
davidalb97 <davidalb97@hotmail.com>
2020-05-14 23:47:01 +0100

    httpserver: Fixed ReportDto.submitter_id param with old name

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/dto/ReportDto.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/dto/ReportDto.kt
@@ -1,7 +1,7 @@
 package pt.isel.ps.g06.httpserver.dataAccess.db.dto
 
 data class ReportDto(
-        val report_submission_id: Int,
+        val submitter_id: Int,
         val submission_id: Int,
         val description: String
 )
