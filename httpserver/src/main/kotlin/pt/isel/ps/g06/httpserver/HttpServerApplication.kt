28b05081e54be21b166e1f76c96424906539548e
Pedro Pires <pedrokase@hotmail.com>
2020-05-15 09:56:42 +0100

    Spring bug fix

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/HttpServerApplication.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/HttpServerApplication.kt
@@ -1,7 +1,12 @@
 package pt.isel.ps.g06.httpserver
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
+
+@EnableWebMvc
+@SpringBootApplication
 class HttpServerApplication
 
 fun main(args: Array<String>) {
