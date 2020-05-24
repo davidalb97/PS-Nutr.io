040c3bfe0bf8a403ff6de133194b608176f60f0e
davidalb97 <davidalb97@hotmail.com>
2020-05-24 23:43:31 +0100

    httpserver: RestaurantApiType & FoodApiType no longer check for nulls on .getOrDefault() as .equals() already does it. Added .getOrNull()

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/api/food/FoodApiType.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/api/food/FoodApiType.kt
@@ -1,16 +1,16 @@
 package pt.isel.ps.g06.httpserver.dataAccess.api.food
 
+
 enum class FoodApiType {
     Spoonacular;
 
     companion object {
         fun getOrDefault(type: String?, default: FoodApiType = Spoonacular): FoodApiType {
-            return try {
-                return if (type == null) default
-                else valueOf(type)
-            } catch (e: IllegalArgumentException) {
-                default
-            }
+            return values().find { it.toString().equals(type, true) } ?: default
+        }
+
+        fun getOrNull(type: String?): FoodApiType? {
+            return values().find { it.toString().equals(type, true) }
         }
     }
 }
