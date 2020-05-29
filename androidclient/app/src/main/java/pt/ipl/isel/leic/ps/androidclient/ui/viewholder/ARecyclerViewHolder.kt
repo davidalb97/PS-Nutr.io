b8bf7cdbb8f171b46c67c4fb4497f545b684d6fb
Mig07 <miguelfluis7@gmail.com>
2020-05-29 21:04:43 +0100

    Removed some comments

--- androidclient/app/src/main/java/pt/ipl/isel/leic/ps/androidclient/ui/viewholder/ARecyclerViewHolder.kt
+++ androidclient/app/src/main/java/pt/ipl/isel/leic/ps/androidclient/ui/viewholder/ARecyclerViewHolder.kt
@@ -1,15 +1,9 @@
 package pt.ipl.isel.leic.ps.androidclient.ui.viewholder
 
 import android.content.Context
-import android.os.Bundle
 import android.os.Parcelable
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.Toast
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import pt.ipl.isel.leic.ps.androidclient.R
 
 /**
  * A generic View Holder for Recycler Lists
@@ -20,36 +14,8 @@ abstract class ARecyclerViewHolder<T : Parcelable>(
 ) : RecyclerView.ViewHolder(view) {
 
     lateinit var item: T
-    //lateinit var addToCalculatorButton: ImageButton
 
     open fun bindTo(item: T) {
         this.item = item
     }
-
-    /*override fun onClick(v: View?) {
-        turnButtonsInvisible()
-    }
-
-    override fun onLongClick(v: View?): Boolean {
-        turnButtonsVisible()
-        setupAddToCalculatorButton()
-        return true
-    }
-
-    fun setupAddToCalculatorButton() {
-        addToCalculatorButton.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putParcelable("bundledMeal", this.item)
-            turnButtonsInvisible()
-            view.findNavController().navigate(R.id.nav_calculator, bundle)
-        }
-    }
-
-    open fun turnButtonsVisible() {
-        addToCalculatorButton.visibility = View.VISIBLE
-    }
-
-    open fun turnButtonsInvisible() {
-        addToCalculatorButton.visibility = View.INVISIBLE
-    }*/
 }
