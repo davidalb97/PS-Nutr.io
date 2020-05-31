2632b8a760f4875aaa9f87542df98dc8d612db68
Mig07 <miguelfluis7@gmail.com>
2020-05-31 18:01:45 +0100

    Added some insulin calculator tests

--- androidclient/app/src/test/java/pt/ipl/isel/leic/ps/androidclient/InsulinCalculatorTests.kt
+++ androidclient/app/src/test/java/pt/ipl/isel/leic/ps/androidclient/InsulinCalculatorTests.kt
@@ -1,11 +1,64 @@
 package pt.ipl.isel.leic.ps.androidclient
 
+import org.junit.Assert
 import org.junit.Test
+import pt.ipl.isel.leic.ps.androidclient.data.db.InsulinCalculator
+import pt.ipl.isel.leic.ps.androidclient.data.db.dto.InsulinProfileDto
 
 class InsulinCalculatorTests {
+
+    private val insulinCalculator = InsulinCalculator()
 
     @Test
     fun insulinCalculatorTest1() {
 
+        // A insulin profile mock
+        val insulinProfileMock =
+            InsulinProfileDto(
+                "ProfileTest1",
+                "00:00",
+                "23:59",
+                110,
+                50,
+                25
+            )
+
+        val currentBloodGlucose = 151
+        val mealCarbsMock = 15
+
+        Assert.assertEquals(
+            1.42f, // expected 1.42 insulin doses to be injected
+            insulinCalculator.calculateMealInsulin(
+                insulinProfileMock,
+                currentBloodGlucose,
+                mealCarbsMock
+            )
+        )
+    }
+
+    @Test
+    fun insulinCalculatorTest2() {
+        // A insulin profile mock
+        val insulinProfileMock =
+            InsulinProfileDto(
+                "ProfileTest1",
+                "00:00",
+                "23:59",
+                100,
+                30,
+                15
+            )
+
+        val currentBloodGlucose = 70
+        val mealCarbsMock = 40
+
+        Assert.assertEquals(
+            1.67f, // expected 1.42 insulin doses to be injected
+            insulinCalculator.calculateMealInsulin(
+                insulinProfileMock,
+                currentBloodGlucose,
+                mealCarbsMock
+            )
+        )
     }
 }
