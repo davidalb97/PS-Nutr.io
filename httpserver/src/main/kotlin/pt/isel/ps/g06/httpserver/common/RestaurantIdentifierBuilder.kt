235b0521410232e38396b315d4122f6c1ce5ed4d
Pedro Pires <pedrokase@hotmail.com>
2020-06-11 16:46:55 +0100

    Added proper exception for invalid RestaurantIdentifier

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/common/RestaurantIdentifierBuilder.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/common/RestaurantIdentifierBuilder.kt
@@ -1,6 +1,7 @@
 package pt.isel.ps.g06.httpserver.common
 
 import org.springframework.stereotype.Component
+import pt.isel.ps.g06.httpserver.common.exception.MalformedRestaurantIdentifierException
 import pt.isel.ps.g06.httpserver.model.RestaurantIdentifier
 
 const val ID_SEPARATOR = "+"
@@ -8,20 +9,30 @@ private const val NUMBER_OF_IDS = 3
 
 @Component
 class RestaurantIdentifierBuilder {
-    //TODO Better exception: MalformedInput/MalformedIdentifier whenever exception is thrown
     fun extractIdentifiers(restaurantId: String): RestaurantIdentifier {
         val values = restaurantId.split(ID_SEPARATOR, limit = NUMBER_OF_IDS)
 
         if (values.size != NUMBER_OF_IDS) {
-            throw IllegalStateException("Cannot extract identifiers from given string!")
+            throw MalformedRestaurantIdentifierException(
+                    """
+                    Cannot extract all identifiers from given String!
+                    Required: $NUMBER_OF_IDS
+                    Found: ${values.size}
+                    """
+            )
         }
 
-        val submitterId = values[0].toIntOrNull() ?: throw IllegalStateException()
+        val submitterId = values[0]
+                .toIntOrNull()
+                ?: throw MalformedRestaurantIdentifierException("Given submitter is not a number!")
 
         val submissionId = if (values[1].isBlank()) null
-        else values[2].toIntOrNull() ?: throw IllegalStateException()
+        else values[1]
+                .toIntOrNull()
+                ?: throw MalformedRestaurantIdentifierException("Given Submission is not a number!")
 
-        val apiId = if (values[2].isBlank()) null else values[2]
+        val apiId = if (values[2].isBlank()) null
+        else values[2]
 
         return RestaurantIdentifier(
                 submitterId = submitterId,
---
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/common/exception/MalformedRestaurantIdentifierException.kt
@@ -0,0 +1,3 @@
+package pt.isel.ps.g06.httpserver.common.exception
+
+class MalformedRestaurantIdentifierException(val detail: String) : Exception(detail)
