84541b476cbf107fb701fb266e287943521e19f2
davidalb97 <davidalb97@hotmail.com>
2020-06-14 13:49:30 +0100

    androidClient: Back on custom meal creation

--- androidclient/app/src/main/java/pt/ipl/isel/leic/ps/androidclient/ui/fragment/constant/AddCustomMealFragment.kt
+++ androidclient/app/src/main/java/pt/ipl/isel/leic/ps/androidclient/ui/fragment/constant/AddCustomMealFragment.kt
@@ -11,6 +11,7 @@ import android.widget.EditText
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import pt.ipl.isel.leic.ps.androidclient.R
 import pt.ipl.isel.leic.ps.androidclient.data.db.entity.DbMealInfoEntity
 import pt.ipl.isel.leic.ps.androidclient.data.model.Cuisine
@@ -85,7 +86,9 @@ class AddCustomMealFragment : Fragment() {
                         portions = emptyList(),
                         isSuggested = false
                     )
-                ).execute()
+                ).setOnPostExecute {
+                    view.findNavController().navigate(R.id.nav_tab_your_meals)
+                }.execute()
             }
         }
     }
