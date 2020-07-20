4b6ea8bae8d63ac27afbe0bf93844208ce288347
davidalb97 <davidalb97@hotmail.com>
2020-07-20 20:08:15 +0100

    httpserver: OffsetDateTimeFormatter

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/mapper/DbRowSubmissionMapper.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/db/mapper/DbRowSubmissionMapper.kt
@@ -4,16 +4,18 @@ import org.jdbi.v3.core.mapper.RowMapper
 import org.jdbi.v3.core.statement.StatementContext
 import pt.isel.ps.g06.httpserver.dataAccess.db.dao.SubmissionDao
 import pt.isel.ps.g06.httpserver.dataAccess.db.dto.DbSubmissionDto
-import pt.isel.ps.g06.httpserver.util.parsePostgresql
+import pt.isel.ps.g06.httpserver.util.OffsetDateTimeSupport
 import java.sql.ResultSet
 
-class SubmissionMapper: RowMapper<DbSubmissionDto> {
+class DbRowSubmissionMapper: RowMapper<DbSubmissionDto> {
 
     override fun map(rs: ResultSet?, ctx: StatementContext?): DbSubmissionDto {
         return DbSubmissionDto(
-                rs!!.getInt(SubmissionDao.id),
-                rs.getString(SubmissionDao.type),
-                parsePostgresql(rs.getString(SubmissionDao.date))
+                submission_id = rs!!.getInt(SubmissionDao.id),
+                submission_type = rs.getString(SubmissionDao.type),
+                submission_date = OffsetDateTimeSupport.parseFromDateTimeWithWithTimeZone(
+                        rs.getString(SubmissionDao.date)
+                )
         )
     }
 }
--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/util/OffsetDateTimeFormatter.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/util/OffsetDateTimeFormatter.kt
@@ -4,13 +4,22 @@ import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
 
-fun parsePostgresql(dateTimeWithTimeZone: String): OffsetDateTime {
-    val formatter = DateTimeFormatterBuilder()
-            .append(DateTimeFormatter.ISO_DATE)
-            .appendLiteral(' ')
-            .appendPattern("HH:mm:ss")
-            .appendPattern("[.SSSSSSSSS][.SSSSSSS][.SSSSSSS][.SSSSSS][.SSSSS][.SSSS][.SSS][.SS][.S]")
-            .appendOffset("+HH", "Z")
-            .toFormatter()
-    return OffsetDateTime.parse(dateTimeWithTimeZone, formatter)
+class OffsetDateTimeSupport {
+
+    companion object {
+        /**
+         * Parses a PostgreSQL date time with time zone formatted string into an OffsetDateTime
+         * @param dateTimeWithTimeZone A PostgreSQL date time with time zone formatted string
+         */
+        fun parseFromDateTimeWithWithTimeZone(dateTimeWithTimeZone: String): OffsetDateTime {
+            val formatter = DateTimeFormatterBuilder()
+                    .append(DateTimeFormatter.ISO_DATE)
+                    .appendLiteral(' ')
+                    .appendPattern("HH:mm:ss")
+                    .appendPattern("[.SSSSSSSSS][.SSSSSSS][.SSSSSSS][.SSSSSS][.SSSSS][.SSSS][.SSS][.SS][.S]")
+                    .appendOffset("+HH", "Z")
+                    .toFormatter()
+            return OffsetDateTime.parse(dateTimeWithTimeZone, formatter)
+        }
+    }
 }
