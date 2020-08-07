57b79b24aa0c82851a2326f72452d21486de193b
mig07 <miguelfluis7@gmail.com>
2020-08-07 12:38:19 +0100

    Solved last issues from the pull request

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/input/UserRegisterInput.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/input/UserRegisterInput.kt
@@ -15,6 +15,6 @@ class UserRegisterInput(
         @field:NotBlank(message = "An username must be given!")
         val username: String,
 
-        @field:NotBlank(message = "An password must be given!")
+        @field:NotBlank(message = "A password must be given!")
         val password: String
 )
