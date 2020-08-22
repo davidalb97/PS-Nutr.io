b34679ebc42aad0d7b14f724182b97a932d46004
davidalb97 <davidalb97@hotmail.com>
2020-08-22 18:03:03 +0100

    androidclient: Updated custom meal/restaurant output

--- androidclient/app/src/main/java/pt/ipl/isel/leic/ps/androidclient/data/api/dto/output/CustomMealOutput.kt
+++ androidclient/app/src/main/java/pt/ipl/isel/leic/ps/androidclient/data/api/dto/output/CustomMealOutput.kt
@@ -1,10 +1,12 @@
 package pt.ipl.isel.leic.ps.androidclient.data.api.dto.output
 
+import android.net.Uri
 
-data class MealOutput(
+data class CustomMealOutput(
     val name: String,
     val quantity: Int,
     val unit: String,
     val ingredients: Iterable<IngredientOutput>,
-    val cuisines: Iterable<String>
+    val cuisines: Iterable<String>,
+    val imageUri: Uri?
 )
