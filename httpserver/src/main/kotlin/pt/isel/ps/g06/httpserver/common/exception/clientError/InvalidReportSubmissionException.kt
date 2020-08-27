15d6ab2246efad052de318cf889af0e6b1b7129b
davidalb97 <davidalb97@hotmail.com>
2020-08-27 21:55:18 +0100

    httpserver: Fixed InvalidReportSubmissionException title

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/common/exception/clientError/InvalidReportSubmissionException.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/common/exception/clientError/InvalidReportSubmissionException.kt
@@ -3,7 +3,7 @@ package pt.isel.ps.g06.httpserver.common.exception.clientError
 import org.springframework.http.HttpStatus
 import pt.isel.ps.g06.httpserver.common.exception.BaseResponseStatusException
 
-class InvalidReportSubmissionException (title: String = "This submission is not a report")
+class InvalidReportSubmissionException (title: String = "This submission is not reportable")
     : BaseResponseStatusException(
         status = HttpStatus.BAD_REQUEST,
         title = title
