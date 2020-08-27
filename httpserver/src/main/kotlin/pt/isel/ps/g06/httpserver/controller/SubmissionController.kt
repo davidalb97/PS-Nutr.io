7647db53a38db05be78d252f9d0bd4487341ede5
Mig07 <miguelfluis7@gmail.com>
2020-08-27 01:06:34 +0100

    Added SubmissionController with DELETE /submission/:submissionId endpoint

---
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/controller/SubmissionController.kt
@@ -0,0 +1,28 @@
+package pt.isel.ps.g06.httpserver.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import pt.isel.ps.g06.httpserver.common.SUBMISSION
+import pt.isel.ps.g06.httpserver.common.SUBMISSION_ID_VALUE
+import pt.isel.ps.g06.httpserver.model.User
+import pt.isel.ps.g06.httpserver.service.SubmissionService
+import pt.isel.ps.g06.httpserver.service.UserService
+
+@RestController
+class SubmissionController(
+        private val submissionService: SubmissionService
+) {
+
+    @DeleteMapping(SUBMISSION)
+    fun deleteSubmission(
+            @PathVariable(SUBMISSION_ID_VALUE) submissionId: Int,
+            user: User
+    ): ResponseEntity<Void> {
+
+        submissionService.deleteSubmission(submissionId, user)
+
+        return ResponseEntity.ok().build()
+    }
+}
