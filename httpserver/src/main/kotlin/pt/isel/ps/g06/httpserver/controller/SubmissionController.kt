1ff035e52c41c1691a9419c122e0298e46033501
Mig07 <miguelfluis7@gmail.com>
2020-08-27 15:08:13 +0100

    DELETE /submission/:submissionId endpoint working with submissionId parser

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/controller/SubmissionController.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/controller/SubmissionController.kt
@@ -7,21 +7,38 @@ import org.springframework.web.bind.annotation.RestController
 import pt.isel.ps.g06.httpserver.common.SUBMISSION
 import pt.isel.ps.g06.httpserver.common.SUBMISSION_ID_VALUE
 import pt.isel.ps.g06.httpserver.model.User
+import pt.isel.ps.g06.httpserver.service.RestaurantService
 import pt.isel.ps.g06.httpserver.service.SubmissionService
 
 @RestController
 class SubmissionController(
-        private val submissionService: SubmissionService
+        private val submissionService: SubmissionService,
+        private val restaurantService: RestaurantService
 ) {
 
     @DeleteMapping(SUBMISSION)
     fun deleteSubmission(
-            @PathVariable(SUBMISSION_ID_VALUE) submissionId: Int,
+            @PathVariable(SUBMISSION_ID_VALUE) submissionId: String,
             user: User
     ): ResponseEntity<Void> {
 
-        submissionService.deleteSubmission(submissionId, user)
+        val parsedSubmissionId = checkAndParseId(submissionId)
+
+        submissionService.deleteSubmission(parsedSubmissionId, user)
 
         return ResponseEntity.ok().build()
+    }
+
+    /**
+     * If the submissionId is from a restaurant then parsing will occur,
+     * if not just convert to int and return
+     * @param   submissionId    Submission's identifier
+     */
+    private fun checkAndParseId(submissionId: String): Int {
+        if (submissionId.contains("+")) {
+            return submissionId.split("+")[1].toInt()
+        }
+
+        return submissionId.toInt()
     }
 }
