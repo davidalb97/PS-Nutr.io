ebefd89f0a3628e4901ec4b49e49f0c5998341f6
Pedro Pires <pedrokase@hotmail.com>
2020-08-29 08:23:18 +0100

    Added missing validation of ingredients for meal input

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/input/meal/MealInput.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/input/meal/MealInput.kt
@@ -1,6 +1,7 @@
 package pt.isel.ps.g06.httpserver.dataAccess.input.meal
 
 import pt.isel.ps.g06.httpserver.dataAccess.input.ingredient.IngredientInput
+import javax.validation.Valid
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
@@ -14,6 +15,7 @@ data class MealInput(
         @field:Pattern(regexp = "(gr)", message = "You must provide a valid unit! Allowed ones are: 'gr'")
         val unit: String?,
         @field:NotEmpty(message = "You must give at least one ingredient!")
+        @field:Valid
         val ingredients: Collection<IngredientInput>?,
         @field:NotEmpty(message = "You must give at least one cuisine!")
         val cuisines: Collection<String>?
