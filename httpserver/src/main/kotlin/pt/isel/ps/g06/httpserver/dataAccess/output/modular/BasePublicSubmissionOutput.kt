2e383c06ee36c546aea9fb1d836f16805d940afe
davidalb97 <davidalb97@hotmail.com>
2020-09-02 22:31:26 +0100

    httpserver: BasePublicSubmissionOutput now extends output super

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/output/modular/BasePublicSubmissionOutput.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/output/modular/BasePublicSubmissionOutput.kt
@@ -1,6 +1,5 @@
 package pt.isel.ps.g06.httpserver.dataAccess.output.modular
 
-import pt.isel.ps.g06.httpserver.model.modular.BaseSubmission
 import java.net.URI
 
 open class BasePublicSubmissionOutput<Id>(
@@ -8,6 +7,6 @@ open class BasePublicSubmissionOutput<Id>(
         val image: URI?,
         override val name: String,
         override val favorites: FavoritesOutput
-) : BaseSubmission<Id>(
+) : BaseSubmissionOutput<Id>(
         identifier = identifier
 ), INameableOutput, IFavorableOutput
