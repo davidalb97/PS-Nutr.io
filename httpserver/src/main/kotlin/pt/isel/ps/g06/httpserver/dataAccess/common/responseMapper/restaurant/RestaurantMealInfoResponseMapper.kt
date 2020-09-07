c33858185aea1d05d6efbbe05f61a12904a54355
davidalb97 <davidalb97@hotmail.com>
2020-09-08 00:30:40 +0100

    httpserver: Added missing userId, removed old debug code

--- httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/common/responseMapper/restaurant/RestaurantMealInfoResponseMapper.kt
+++ httpserver/src/main/kotlin/pt/isel/ps/g06/httpserver/dataAccess/common/responseMapper/restaurant/RestaurantMealInfoResponseMapper.kt
@@ -27,9 +27,6 @@ class DbRestaurantMealInfoResponseMapper(
 ) : ResponseMapper<DbRestaurantMealDto, MealRestaurantInfo> {
 
     override fun mapTo(dto: DbRestaurantMealDto): MealRestaurantInfo {
-        if(dto.meal_submission_id == 255) {
-            println()
-        }
         val isReportable = lazy {
 
             dto.submission_id?.let {
